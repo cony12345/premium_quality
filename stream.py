4df27da7e0184dbddc404602eccc0bb79f108f96
cony12345 <144059623+cony12345@users.noreply.github.com>
2023-09-18 18:13:13 -0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -3,17 +3,23 @@ import streamlit as st
 
 st.title("Encuesta :) ")
 
-st.write("selecciona un objeto que tenga este color")
+st.write("selecciona un objeto que tenga este color o parecido")
 st.image("verde.png", use_column_width=True)
 
 
 option = st.selectbox('Selecciona tu opción',
     ("palta", "arbol", "rana", "manzana"))
+if option == "palta":
+  st.write("gracias por responder :)")
 
-if option == "rana":
-    st.write ("Gracias por responder :)")
-else: 
-    st.write("Gracias por responder :)")
+elif option == "arbol":
+  st.write("gracias por tu respuesta :)")
+
+elif option == "rana":
+  st.write("gracias por colaborar en la encuesta :)")
+else:
+  st.write("muchas gracias por responder :)")
+    
     
     
 
